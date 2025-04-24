7e9bd90a355d539cc3cf84c0c02c6545481718a6
sauoro <sqwxro@gmail.com>
2025-04-23 20:30:05 -0700

    feat: added UnconnectedPing

--- src/raknet/protocol/packet.rs
+++ src/raknet/protocol/packet.rs
@@ -4,6 +4,7 @@ pub struct PacketId {}
 
 impl PacketId {
     pub const CONNECTED_PING: u8 = 0x00;
+    pub const UNCONNECTED_PING: u8 = 0x01;
 }
 
 pub trait Packet: Sized {
@@ -59,3 +60,33 @@ impl ConnectedPing {
 }
 
 implement_packet!(ConnectedPing, PacketId::CONNECTED_PING);
+
+pub struct UnconnectedPing {
+    pub client_timestamp: i64,
+    pub client_guid: u64,
+}
+
+impl UnconnectedPing {
+    pub fn encode(&self, writer: &mut impl BinaryWriter) -> BinaryResult<()> {
+        writer.write_i64_be(self.client_timestamp)?;
+        writer.write_magic()?;
+        writer.write_u64_be(self.client_guid)?;
+        Ok(())
+    }
+
+    pub fn decode(reader: &mut impl BinaryReader) -> BinaryResult<Self> {
+        let client_timestamp = reader.read_i64_be()?;
+        if !reader.read_magic()? {
+            return Err(BinaryError::InvalidData(
+                "Invalid magic sequence in UnconnectedPing".into(),
+            ));
+        }
+        let client_guid = reader.read_u64_be()?;
+        Ok(Self {
+            client_timestamp,
+            client_guid,
+        })
+    }
+}
+
+implement_packet!(UnconnectedPing, PacketId::UNCONNECTED_PING);
