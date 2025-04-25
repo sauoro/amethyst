5d258342b05c40115aa8094566541656c93a56ee
sauoro <sqwxro@gmail.com>
2025-04-24 17:26:34 -0700

    feat: raknet

--- crates/raknet/src/lib.rs
+++ crates/raknet/src/lib.rs
@@ -3,6 +3,10 @@ pub mod error;
 pub mod reliability;
 pub mod consts;
 pub mod server;
+pub mod connection;
 
 pub use error::*;
 pub use consts::*;
+pub use server::*;
+pub use connection::*;
+pub use reliability::*;
--- crates/raknet/src/server/listener.rs
+++ crates/raknet/src/server/listener.rs
@@ -1,11 +1,23 @@
+use std::collections::HashMap;
 use std::net::SocketAddr;
+use std::sync::{Arc, Mutex};
 use rand::random;
 use tokio::net::UdpSocket;
-use crate::RaknetError;
+use tokio::sync::mpsc::{Receiver, Sender};
+use tokio::sync::Notify;
+use tokio::sync::oneshot::channel;
+use crate::{Connection, RaknetError};
 
 pub struct RaknetListener {
     pub motd: String,
-    pub guid: u64
+    pub guid: u64,
+    pub version_map: Arc<Mutex<HashMap<String, u8>>>,
+    addr: Option<Arc<UdpSocket>>,
+    listened: bool,
+    // connections: Arc<Mutex<HashMap<SocketAddr, Session>>>,
+    // connection_receiver: Receiver<Connection>,
+    // connection_sender: Sender<Connection>,
+    closed_notify: Arc<Notify>,
 }
 
 impl RaknetListener {
@@ -14,14 +26,22 @@ impl RaknetListener {
             Ok(s) => s,
             Err(_) => return Err(RaknetError::BindAddressError(socket_addr.to_string())),
         };
-        
+
         let server_guid = random::<u64>();
         
+        //let (conn_sender, conn_receiver) = channel::<Connection>();
+
         let listener = Self {
             motd: String::new(),
-            guid: server_guid
+            guid: server_guid,
+            version_map: Arc::new(Mutex::new(HashMap::new())),
+            addr: Some(Arc::new(socket)),
+            listened: false,
+            //connection_sender: conn_sender,
+            //connection_receiver: conn_receiver,
+            closed_notify: Arc::new(Notify::new()),
         };
-        
+
         Ok(listener)
     }
 }
