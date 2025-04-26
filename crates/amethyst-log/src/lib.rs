e3df667046a3d164cbbc5445ecaa196a5b5d3030
chirag <chiraggamer7275@gmail.com>
2025-04-26 09:53:16 +0530

    Update lib.rs
    Better logging

--- crates/amethyst-log/src/lib.rs
+++ crates/amethyst-log/src/lib.rs
@@ -1,29 +1,59 @@
-extern crate core;
+use chrono::Utc;
+use log::{Level, Log, LevelFilter};
+use std::io::{BufWriter, Write};
+use std::sync::Mutex;
 
-use chrono::Local;
-use log::{Level, Log};
+pub struct AmethystLogger {
+    max_level: Level,
+    writer: Mutex<BufWriter<std::io::Stdout>>,
+}
 
-// TODO: make a better logging system.
-pub const AMETHYST_LOGGER: AmethystLogger = AmethystLogger;
+impl AmethystLogger {
+    pub fn new(max_level: Level) -> Self {
+        AmethystLogger {
+            max_level,
+            writer: Mutex::new(BufWriter::new(std::io::stdout())),
+        }
+    }
 
-pub struct AmethystLogger;
+    pub fn init(max_level: Level) -> Result<(), log::SetLoggerError> {
+        let logger = AmethystLogger::new(max_level);
+        log::set_boxed_logger(Box::new(logger))?;
+        log::set_max_level(max_level.to_level_filter());
+        Ok(())
+    }
+}
 
 impl Log for AmethystLogger {
     fn enabled(&self, metadata: &log::Metadata) -> bool {
-        metadata.level() <= Level::Info
+        metadata.level() <= self.max_level
     }
 
     fn log(&self, record: &log::Record) {
         if self.enabled(record.metadata()) {
-            let now = Local::now();
-            println!(
-                "{} {} {}",
-                now.format("%Y-%m-%d %H:%M:%S"),
+            let now = Utc::now();
+            let message = format!(
+                "{} {} [{}] {}\n",
+                now.format("%Y-%m-%d %H:%M:%S%.3f"),
                 record.level(),
+                record.target(),
                 record.args()
             );
+            let mut writer = self.writer.lock().unwrap();
+            writer.write_all(message.as_bytes()).unwrap();
         }
     }
 
-    fn flush(&self) {}
+    fn flush(&self) {
+        let mut writer = self.writer.lock().unwrap();
+        writer.flush().unwrap();
+    }
+}
+
+fn main() {
+    AmethystLogger::init(Level::Info).unwrap();
+    log::info!("Server started");
+    log::debug!("This won't be logged because level is Info");
+    log::error!("An error occurred");
+    log::logger().flush();
 }
